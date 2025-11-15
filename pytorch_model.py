40f9901db22e4c76337c0f0458afe8b0e716205d
Vlad Rusu <148282379+vladmateirusu@users.noreply.github.com>
2025-11-15 17:13:18 -0500

    start

---
+++ pytorch_model.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import timm
+
+import matplotlib.pyplot as plt # For data viz
+import pandas as pd
+import numpy as np
+import sys
+from tqdm.notebook import tqdm
+
+print('System Version:', sys.version)
+print('PyTorch version', torch.__version__)
+print('Torchvision version', torchvision.__version__)
+print('Numpy version', np.__version__) 
+print('Pandas version', pd.__version__)
+
+class PlayingCardDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data = ImageFolder(data_dir, transform=transform)
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        return self.data[idx]
+    
+    @property
+    def classes(self):
+        return self.data.classes
+
+        
